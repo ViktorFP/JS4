7b425c7d094a942e3ea1c6947a47e088e638251d
ViktorFP <VikPr_85@mail.ru>
2016-06-10 18:46:24 +0300

    change style

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 window.FuncKit=(function(){
+	var arrayPararrahps=[];
+	
 	function docName(){
 		var name="Document Name";
 		var arrayElements=document.getElementsByClassName("doc_name");	
@@ -6,5 +8,34 @@ window.FuncKit=(function(){
 			arrayElements[i].innerHTML = name;
 		}
 	}
-	return {setDocName:docName};
+	
+	function hide(field){
+		var currentClass=field.getAttribute("class");
+		var current=arrayPararrahps[currentClass];
+		if(current){
+			alert(current);
+			}else{
+			var arrayElements=document.getElementsByClassName(currentClass);
+			for(var i=0;i<arrayElements.length;i++){
+				changer(arrayElements[i]);
+			} 
+		}
+		
+	}
+	
+	function changer(element){
+		var tag=element.tagName;
+		switch(tag){
+			case 'INPUT':
+			element.setAttribute('value', '	\u21D3');
+			break;
+			case 'DIV':
+			element.innerHTML ='NewText';
+			element.setAttribute("style", "margin:5px 0; border:solid 2px #B1C7D3; padding:5px; height:100%"); 			
+			break;
+		}
+	}
+	
+	return {setDocName:docName,
+	hideText:hide};
 })();
