82fa222e022a95519e8b3f4556013f5e619473e2
ViktorFP <VikPr_85@mail.ru>
2016-06-11 10:01:41 +0300

    anchor

--- script.js
+++ script.js
@@ -12,16 +12,28 @@ window.FuncKit=(function(){
 					document.getElementsByClassName("doc_name")[0].innerHTML=dataObject[0].name;
 					var body='';
 					for(var i=0;dataObject[i];i++){
+						//create block for document name
 						var obj=dataObject[i];
-						console.log(obj);
-						body+='<div id="'+obj.id+'" class=doc_name">'+obj.name+'</div><ul>';
-						
+						console.log(obj);/////////////////////
+						body+='<div id="'+obj.id+'" class=doc_name">'+(i+1)+' '+obj.name+'</div><ul>';
+						//create fragments list
 						var fragments=obj.fragments;
 						for(var j=0;j<fragments.length;j++){
-							var name=fragments[j].name;
-							body+='<li id="'+i+'_'+j+'"><a href=#'+name+'">'+name+'</a></li> ';
+							var id=i+'_'+j;
+							body+='<li id="'+id+'"><a href="#'+id+'a">'+fragments[j].name+'</a></li> ';							
 						}
 						body+='</ul>'; 
+						//create context for first
+						if(i===0){
+							var fragmentsBlock='';
+							for(var k=0;k<fragments.length;k++){							
+								var idF=i+'_'+k;
+								fragmentsBlock+='<div id="'+idF+'a" class="paragraph"><span class="paragraphName">'+
+								fragments[k].name+'</span><span class="hide"><input class="'+idF+
+								'" type="button" value="&#8657;" onclick="FuncKit.hideText(this)"></span></div><div class="'+idF+'"></div>';
+							}
+							document.getElementsByClassName("content_view")[0].innerHTML=fragmentsBlock;							
+						}
 					}
 					var contentList=document.getElementsByClassName('content_list');
 					contentList[0].innerHTML =body;
@@ -61,7 +73,7 @@ window.FuncKit=(function(){
 			element.setAttribute('value', '	\u21D3');
 			break;
 			case 'DIV':
-			element.innerHTML ='NewText';
+			element.innerHTML ='NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText NewText ';
 			element.setAttribute("style", "margin:5px 0; border:solid 2px #B1C7D3; padding:5px; height:100%"); 			
 			break;
 		}
